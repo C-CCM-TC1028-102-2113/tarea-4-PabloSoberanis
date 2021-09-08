4a189a33d6f4520a9a96ba324165ddc7740166a3
PabloSoberanis <89001142+PabloSoberanis@users.noreply.github.com>
2021-09-08 15:50:07 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -2,6 +2,12 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    num = int(input("Escribe un numero : "))
+    n = 1
+
+    while n **2 <= num:
+        n += 1
+    print(n)
     pass
 
 if __name__=='__main__':
