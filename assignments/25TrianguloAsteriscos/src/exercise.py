840554b446c6485e3425336e26b8b4de04b6d83c
PabloSoberanis <89001142+PabloSoberanis@users.noreply.github.com>
2021-09-08 15:52:16 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,6 +1,13 @@
 
 def main():
     #Escribe tu código debajo de esta línea
+    a = int(input("altura?"))
+    b = a
+
+    for x in range(a):
+    b = b - 1
+    c = "*" * (x+1) 
+    print(" " * b + c)
     pass
 
 
